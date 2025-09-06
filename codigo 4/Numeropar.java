350e17ef1312d43602b8f8b5d1c955796a34bdbd
juanm12-1 <jmanuelmejia@estudiante.uniajc.edu.co>
2025-09-06 21:06:37 +0000

    R//4

---
+++ codigo 4/Numeropar.java
@@ -0,0 +1,15 @@
+public class Numeropar {
+    public static void main(String[] args) {
+        int numero = 56;
+        if (numero % 2==0) {
+            System.out.println(numero + " es un numero par." );
+            }else{ 
+                System.out.println(numero +" es un numero impar.");
+                }
+            
+        }
+        
+    }
+
+    
+
