bc40314ff5a9dcf194e8116909f2151be5554b8f
juanm12-1 <jmanuelmejia@estudiante.uniajc.edu.co>
2025-09-06 21:51:18 +0000

    R//5

---
+++ codigo 5/DivisorDeNumero.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DivisorDeNumero {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero (posible divisor):");
+        int numero1 = entrada.nextInt();
+
+        System.out.println("Ingrese el segundo numero (dividendo):");
+        int numero2 = entrada.nextInt();
+
+        if (numero2 % numero1 == 0) {
+            System.out.println(numero1 + " es divisor de " + numero2);
+        } else {
+            System.out.println(numero1 + " no es divisor de " + numero2);
+        }
+
+        entrada.close();
+    }
+}
