5e376de299094b4f62b3a5ff53e1e684701f0bb6
juanm12-1 <jmanuelmejia@estudiante.uniajc.edu.co>
2025-11-10 20:09:18 +0000

    practica

--- practica/practica/CalculadoraMate.java
+++ practica/practica/CalculadoraMate.java
@@ -3,51 +3,43 @@ package practica;
 import java.util.Scanner;
 
 public class CalculadoraMate {
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double numero1, numero2, resultado;
         char operador;
 
-        System.out.println("Ingrese el primer número:");
+        System.out.print("Ingrese el primer número: ");
         numero1 = scanner.nextDouble();
 
-        System.out.println("Ingrese el operador (+, -, *, /):");
+        System.out.print("Ingrese el operador (+, -, *, /): ");
         operador = scanner.next().charAt(0);
 
-        System.out.println("Ingrese el segundo número:");
+        System.out.print("Ingrese el segundo número: ");
         numero2 = scanner.nextDouble();
 
         switch (operador) {
             case '+':
                 resultado = numero1 + numero2;
-                System.out.println(numero1 + " + " + numero2 + " = " + resultado);
+                System.out.println("Resultado: " + resultado);
                 break;
-
             case '-':
                 resultado = numero1 - numero2;
-                System.out.println(numero1 + " - " + numero2 + " = " + resultado);
+                System.out.println("Resultado: " + resultado);
                 break;
-
             case '*':
                 resultado = numero1 * numero2;
-                System.out.println(numero1 + " * " + numero2 + " = " + resultado);
+                System.out.println("Resultado: " + resultado);
                 break;
-
             case '/':
                 if (numero2 != 0) {
                     resultado = numero1 / numero2;
-                    System.out.println(numero1 + " / " + numero2 + " = " + resultado);
+                    System.out.println("Resultado: " + resultado);
                 } else {
                     System.out.println("Error: no se puede dividir entre cero.");
                 }
                 break;
-
             default:
                 System.out.println("Operación no válida.");
-                break;
         }
 
         scanner.close();
